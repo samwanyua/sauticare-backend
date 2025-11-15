b2f2631b562fb5b0810c643a91547008366f2e6c
samwanyua <samexample8@gmail.com>
2025-11-16 01:14:25 +0300

    fix auth

--- api/auth.py
+++ api/auth.py
@@ -8,10 +8,10 @@ import requests
 import os
 from fastapi import Body, Form
 
-from api.schemas.auth import UserCreate, UserLogin, TokenResponse
-from api.db.database import SessionLocal
-from api.db.models import User
-from api.utils.security import hash_password, verify_password, create_access_token, decode_access_token
+from .schemas.auth import UserCreate, UserLogin, TokenResponse
+from .db.database import SessionLocal
+from .db.models import User
+from .utils.security import hash_password, verify_password, create_access_token, decode_access_token
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
