55da1cd2d06cc4d93b815a31a62a1560bd70889c
samwanyua <samexample8@gmail.com>
2025-11-16 00:31:16 +0300

    fix auth

--- api/auth.py
+++ api/auth.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from urllib.parse import urlencode
 import requests
 import os
-from schemas.auth import UserCreate
+from .schemas.auth import UserCreate
 from fastapi import Body, Form
 
 from db.database import SessionLocal
