9f4356beb6c9261a0e1f1ffaca6362adbd8726fc
samwanyua <samexample8@gmail.com>
2025-11-16 00:39:55 +0300

    fix auth

--- api/auth.py
+++ api/auth.py
@@ -6,12 +6,12 @@ from sqlalchemy.orm import Session
 from urllib.parse import urlencode
 import requests
 import os
-from .schemas.auth import UserCreate
+from api.schemas.auth import UserCreate
 from fastapi import Body, Form
 
-from .db.database import SessionLocal
-from .db.models import User
-from .utils.security import hash_password, verify_password, create_access_token, decode_access_token
+from api.db.database import SessionLocal
+from api.db.models import User
+from api.utils.security import hash_password, verify_password, create_access_token, decode_access_token
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
