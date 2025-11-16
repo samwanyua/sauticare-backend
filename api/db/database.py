27ecaa0c63031a29d35763f623c76b6cca26f8a1
samwanyua <samexample8@gmail.com>
2025-11-16 08:00:41 +0300

    Update ci

--- api/db/database.py
+++ api/db/database.py
@@ -1,7 +1,7 @@
 # app/db/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from api.core.config import settings
+from core.config import settings
 
 # Create SQLAlchemy engine
 engine = create_engine(
