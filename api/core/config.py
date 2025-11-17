77aca1883d1c9f2e237621f12891032d66c831a6
samwanyua <samexample8@gmail.com>
2025-11-17 22:05:13 +0300

    Fix neondb serverless

--- api/core/config.py
+++ api/core/config.py
@@ -1,23 +1,20 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    
     PROJECT_NAME: str = "SautiCare Backend"
-
-    # Database
-    DATABASE_URL: str = "sqlite:///./sauticare.db"
+    DATABASE_URL: str
 
     # JWT
     SECRET_KEY: str = "supersecret"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
-    # Hugging Face (Set later)
+    # Hugging Face
     HF_API_KEY: str = ""
     HF_STT_MODEL: str = ""
     HF_TTS_MODEL: str = ""
 
     class Config:
         env_file = ".env"
-
+        extra = "allow"  
 settings = Settings()
--- api/utils/security.py
+++ api/utils/security.py
@@ -1,16 +1,20 @@
+# api/utils/security.py
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
-from jose import jwt
-from api.core.config import settings
 from jose import jwt, JWTError
+from api.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
-def hash_password(password: str):
-    return pwd_context.hash(password)
 
-def verify_password(plain: str, hashed: str):
-    return pwd_context.verify(plain, hashed)
+def hash_password(password: str) -> str:
+    # truncate to 72 bytes for bcrypt
+    safe_password = password.encode("utf-8")[:72]
+    return pwd_context.hash(safe_password)
+
+def verify_password(plain: str, hashed: str) -> bool:
+    safe_plain = plain.encode("utf-8")[:72]
+    return pwd_context.verify(safe_plain, hashed)
 
 def create_access_token(data: dict, expires_delta=None):
     to_encode = data.copy()
@@ -23,6 +27,6 @@ def create_access_token(data: dict, expires_delta=None):
 def decode_access_token(token: str):
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-        return payload  # this will contain "sub" and "exp"
+        return payload  # contains "sub" and "exp"
     except JWTError:
         return None
